bc0e42e2cc90907aa6618bbb5dc29afaf481a3d2
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2020-09-27 19:57:40 -0500

    Update data to data folder

--- case_study_3/get_data.sh
+++ case_study_3/get_data.sh
@@ -2,6 +2,8 @@
 
 # get the zip file
 wget http://www.rdatasciencecases.org/Spam/SpamAssassinMessages.zip
+mkdir data
+mv ./SpamAssassinMessages.zip ./data/
 # quietly unzip and remove zip file
-unzip -q SpamAssassinMessages.zip
-rm -f SpamAssassinMessages.zip
+unzip -q ./data/SpamAssassinMessages.zip -d ./data
+rm -f ./data/SpamAssassinMessages.zip
